32b695220afa6c77dd44f7d8d8b67f07b676e1d1
Yash Naik <yashtnaik@gmail.com>
2025-03-18 15:38:38 +0530

    aes

--- modules/create_s3/main.tf
+++ modules/create_s3/main.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "SSE-s3-bucket" {
     bucket = aws_s3_bucket.bucket_for_statelock.bucket
     rule {
         apply_server_side_encryption_by_default {
-          sse_algorithm = "AES-256"
+          sse_algorithm = "AES256"
         }
       }
 
