5b8f864bf036079b10422bebb10e2d0af4daa270
Yash Naik <yashtnaik@gmail.com>
2025-03-18 15:10:45 +0530

    ffff

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "create_s3" {
-    source = "/modules/create_s3"
+    source = "./Modules/create_s3"
     bucket_name = var.root_bucket_name
 }
 
 module "create_dynamoDB" {
-    source = "/modules/create_dynamoDB"
+    source = "./Modules/create_dynamoDB"
     dynamodb_table_name = var.root_dynamo_name
   
 }
