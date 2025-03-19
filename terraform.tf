60d01f1b5b44e05a2032a25ba6971b6d8d5829db
Yash Naik <yashtnaik@gmail.com>
2025-03-19 20:20:26 +0530

    dfdf

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "s3-bucket-backend-yashtnaik1"
-#     key = "yash/terraform.tfstate"
-#     use_lockfile = true
-#     region = "ap-south-1"
-#     dynamodb_table = "dynamanoDB_for_backend"
-#     encrypt = true 
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "s3-bucket-backend-yashtnaik1"
+    key = "yash/terraform.tfstate"
+    use_lockfile = true
+    region = "ap-south-1"
+    dynamodb_table = "dynamanoDB_for_backend"
+    encrypt = true 
+  }
+}
